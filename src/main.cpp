9d585fc4b04cd20563341619d09ae450b334f7aa
Chris <chris_hulme99@outlook.com>
2020-11-11 21:24:40 +0400

    Added facial recognition.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <read_camera_stream.h>
 #include <yarp/sig/Image.h>
+#include <linear_filters.h>
+#include <facial_and_object_detection.h>
 
 using namespace yarp::sig;
 
 int main()
 {
-    ImageOf<PixelRgb> *image = read_camera_stream();
+    //ImageOf<PixelRgb> *image = read_camera_stream();
+    apply_and_display_filtered_images();
+    cv::Mat image = read_image("man.jpeg");
+    facial_detection(image);
+
     return 0;
 }
