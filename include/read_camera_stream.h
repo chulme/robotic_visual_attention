5238875ed17ae536b781da7e32f4143d2a389ec4
cyberFerret01 <cyberFerrets@gmail.com>
2020-11-23 19:06:57 +0000

    codeWorkingWithX2GO

--- include/read_camera_stream.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <yarp/sig/Image.h>
-#include <yarp/os/BufferedPort.h>
-
-yarp::sig::ImageOf<yarp::sig::PixelRgb> read_camera_stream();
-yarp::sig::ImageOf<yarp::sig::PixelRgb> read_port_until_image_received(
-    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> &imagePort);
--- include/utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string get_project_path();
-cv::Mat read_image(const std::string &image_name);
